9175931521eb4af99b0089891351a1981ca559f6
M SAI PRANEETH <56910706+randomness11@users.noreply.github.com>
2020-03-21 15:49:56 +0530

    Add files via upload

---
+++ BFC.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 13:00:55 2020
+
+@author: SaiPr
+"""
+
+import split_folders
+
+split_folders.ratio('D:/spyder file/Binary Face classifier/Pos',output="D:/spyder file/Binary Face classifier/Test2",seed = 1337 ,ratio = (0.8,0.2))
+
+
+from keras.models import Sequential 
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten 
+from keras.layers import Dense 
+
+classifier = Sequential()
+#building the model 
+classifier.add(Convolution2D(32,3,3,input_shape = (64,64,3),activation = 'relu'))
+# 32 is number of feature maps , 3,3 is the size of feature detector , we give input size of our image
+classifier.add(Convolution2D(16,3,3,input_shape = (64,64,3),activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2,2)) )
+classifier.add(Flatten())
+#now make the ANN
+classifier.add(Dense(output_dim =128 , activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+#compile the model 
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics=['accuracy'])
+#adam is like gradient desecnt where loss function is logisitic thing
+
+from keras.preprocessing.image import ImageDataGenerator 
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+        'Train',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+        'Ftest',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=1000,
+        epochs=1,
+        validation_data=test_set,
+        nb_val_samples=1112)
+
+#predicting a new image 
+import numpy as np 
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+img = plt.imread('image10.jpg')
+plt.imshow(img)
+reimg = resize(img, (64,64,3)) 
+img = plt.imshow(reimg) 
+pred = classifier.predict(np.array( [reimg,] ))
+print(pred)
