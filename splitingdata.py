3bf83c7e46b2c2d872aa67dcf32857f0b15b0278
M SAI PRANEETH <56910706+randomness11@users.noreply.github.com>
2020-03-21 16:01:42 +0530

    Add files via upload

---
+++ splitingdata.py
@@ -0,0 +1,8 @@
+
+
+# pip install split-folders
+#this is a useful package that can be used to split files containing data
+
+import split_folders
+
+split_folders.ratio('input file location ',output="output file location",seed = 1337 ,ratio = (0.8,0.2))
